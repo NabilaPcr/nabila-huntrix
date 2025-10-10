01e19db655bf3a45714374cfbb530cfe2f8ccee7
NabilaPcr <nabila24si@mahasiswa.pcr.ac.id>
2025-10-10 09:37:49 +0700

    perubahan halaman dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
 
 
@@ -40,5 +41,6 @@ Route::get('/pegawai',[PegawaiController::class,'index']);
 
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
-
+Route::get('dashboard', [DashboardController::class, 'index'])
+        ->nama('dashboard');
 
