793991aaad07ee46676f8fe3cbbfcb5bd71d6dc0
NabilaPcr <nabila24si@mahasiswa.pcr.ac.id>
2025-10-17 08:04:00 +0700

    perubahan pada form action

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PelangganController;
 
 
 Route::get('/', function () {
@@ -43,4 +44,6 @@ Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
 Route::get('dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
 
