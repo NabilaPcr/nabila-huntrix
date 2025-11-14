7f314b7d7e932e6e441b83dd77fae54ff3f0eff0
NabilaPcr <nabila24si@mahasiswa.pcr.ac.id>
2025-11-14 07:47:39 +0700

    letak db::table yang efisien

--- database/seeders/CreatePelangganDummy.php
+++ database/seeders/CreatePelangganDummy.php
@@ -17,14 +17,16 @@ class CreatePelangganDummy extends Seeder
          $faker = Factory::create();
 
     foreach (range(1, 100) as $index) {
-        DB::table('pelanggan')->insert([ // gunanya sama dengan Pleanggan::create, eloquent itu fungsi bawaan dari laravel
+        $pelanggan[] =[
             'first_name' => $faker->firstName,
             'last_name'  => $faker->lastName,
             'birthday'   => $faker->date('Y-m-d', '2005-12-31'),
             'gender'     => $faker->randomElement(['Male', 'Female', 'Other']),
             'email'      => $faker->unique()->safeEmail,
             'phone'      => $faker->phoneNumber,
-        ]);
+        ];
     }
+            DB::table('pelanggan')->insert($pelanggan); // gunanya sama dengan Pleanggan::create, eloquent itu fungsi bawaan dari laravel
+
     }
 }
